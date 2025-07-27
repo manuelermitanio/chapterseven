1b6a23ebb90c3cbfd0721154ddddd18a7681ecc1
Eman Ermitanio <manuel.ermitanio@gmail.com>
2025-07-28 01:46:56 +0800

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  preview: {
+    port: parseInt(process.env.PORT) || 4173,
+    host: '0.0.0.0'
+  }
 });
